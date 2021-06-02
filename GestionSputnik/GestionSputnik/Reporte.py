e27ad6fd4dd6ff34cfdd4c13e2b24cb514330628
zs18015552 <57199582+zs18015552@users.noreply.github.com>
2021-06-01 23:25:37 -0500

    Correciones para 3ra entrega

--- GestionSputnik/GestionSputnik/Reporte.py
+++ GestionSputnik/GestionSputnik/Reporte.py
@@ -26,7 +26,7 @@ class Reporte():
             try: 
                 subprocess.call(['open', archivo.name])
             except:
-                messagebox.showerror('¡Error!','No se pudo abrir dirección URL')
+                messagebox.showerror('¡Error!','No se pudo abrir el archivo.')
 
         archivo.close()
 
@@ -36,71 +36,73 @@ class Reporte():
 
     def generarReporte(self, empleado, fechaInicio, fechaFin, db):
         if not fechaInicio.get_date() > fechaFin.get_date():
-            idEmpleado = empleado.get()[0:7]
-            comandos = db.cursor()
-            query = 'SELECT dia from Horarios WHERE idEmpleado = {} AND estado = 1'.format(idEmpleado)
-            comandos.execute(query)
-            resultados =comandos.fetchall()
+            try:
+                idEmpleado = empleado.get()[0:7]
+                comandos = db.cursor()
+                query = 'SELECT dia from Horarios WHERE idEmpleado = {} AND estado = 1'.format(idEmpleado)
+                comandos.execute(query)
+                resultados =comandos.fetchall()
 
-            diasLaborales = list()
-            listaDiasHorario = list()
-            listaReporte = list()
+                diasLaborales = list()
+                listaDiasHorario = list()
+                listaReporte = list()
 
-            for resultado in resultados:
-                if resultado[0] == 'lunes': diasLaborales.append(0)
-                if resultado[0] == 'martes': diasLaborales.append(1)
-                if resultado[0] == 'miércoles': diasLaborales.append(2)
-                if resultado[0] == 'jueves': diasLaborales.append(3)
-                if resultado[0] == 'viernes': diasLaborales.append(4)
-                if resultado[0] == 'sábado': diasLaborales.append(5)
-                if resultado[0] == 'domingo': diasLaborales.append(6)
+                for resultado in resultados:
+                    if resultado[0] == 'lunes': diasLaborales.append(0)
+                    if resultado[0] == 'martes': diasLaborales.append(1)
+                    if resultado[0] == 'miércoles': diasLaborales.append(2)
+                    if resultado[0] == 'jueves': diasLaborales.append(3)
+                    if resultado[0] == 'viernes': diasLaborales.append(4)
+                    if resultado[0] == 'sábado': diasLaborales.append(5)
+                    if resultado[0] == 'domingo': diasLaborales.append(6)
 
-            for fecha in self.listarFechas(fechaInicio.get_date(), fechaFin.get_date()):
-                for dia in diasLaborales:
-                    if fecha.weekday() == dia:
-                        listaDiasHorario.append(fecha)
-                        listaReporte.append([fecha.strftime("%Y-%m-%d"),"00:00:00","00:00:00",0])
+                for fecha in self.listarFechas(fechaInicio.get_date(), fechaFin.get_date()):
+                    for dia in diasLaborales:
+                        if fecha.weekday() == dia:
+                            listaDiasHorario.append(fecha)
+                            listaReporte.append([fecha.strftime("%Y-%m-%d"),"00:00:00","00:00:00",0])
 
-            query = 'SELECT fecha, horaEntrada, TIME_TO_SEC(horaEntrada), horaSalida, TIME_TO_SEC(horaSalida) FROM Asistencia WHERE idEmpleado = {}'.format(idEmpleado)
-            comandos.execute(query)
-            listaAsistencias = comandos.fetchall()
-            contadorLista = 0
+                query = 'SELECT fecha, horaEntrada, TIME_TO_SEC(horaEntrada), horaSalida, TIME_TO_SEC(horaSalida) FROM Asistencia WHERE idEmpleado = {}'.format(idEmpleado)
+                comandos.execute(query)
+                listaAsistencias = comandos.fetchall()
+                contadorLista = 0
 
-            for dia in listaDiasHorario:
-                for asistencia in listaAsistencias:
-                    if dia==asistencia[0]:
-                        listaReporte.pop(contadorLista)
-                        listaReporte.insert(contadorLista,[dia.strftime("%Y-%m-%d"),str(asistencia[1]),str(asistencia[3]),int((asistencia[4] - asistencia[2] + 900)//3600)])
-                contadorLista += 1
+                for dia in listaDiasHorario:
+                    for asistencia in listaAsistencias:
+                        if dia==asistencia[0]:
+                            listaReporte.pop(contadorLista)
+                            listaReporte.insert(contadorLista,[dia.strftime("%Y-%m-%d"),str(asistencia[1]),str(asistencia[3]),int((asistencia[4] - asistencia[2] + 900)//3600)])
+                    contadorLista += 1
 
-            query = 'SELECT fecha FROM DiaExcepcion WHERE idEmpleado = {} and estado = 1'.format(idEmpleado)
-            comandos.execute(query)
-            listaExcepciones = comandos.fetchall()
+                query = 'SELECT fecha FROM DiaExcepcion WHERE idEmpleado = {} and estado = 1'.format(idEmpleado)
+                comandos.execute(query)
+                listaExcepciones = comandos.fetchall()
 
-            contadorExcepcion = 0
+                contadorExcepcion = 0
 
-            for dia in listaDiasHorario:
-                for diaE in listaExcepciones:
-                    if dia==diaE[0]:
-                        listaReporte.pop(contadorExcepcion)
-                        listaReporte.insert(contadorExcepcion,[dia.strftime("%Y-%m-%d"),"Excepción","",8])
-                contadorExcepcion += 1
+                for dia in listaDiasHorario:
+                    for diaE in listaExcepciones:
+                        if dia==diaE[0]:
+                            listaReporte.pop(contadorExcepcion)
+                            listaReporte.insert(contadorExcepcion,[dia.strftime("%Y-%m-%d"),"Excepción","",8])
+                    contadorExcepcion += 1
 
-            query = 'SELECT fecha FROM DiaFestivo WHERE estado = 1'.format(idEmpleado)
-            comandos.execute(query)
-            listaFestivos = comandos.fetchall()
+                query = 'SELECT fecha FROM DiaFestivo WHERE estado = 1'.format(idEmpleado)
+                comandos.execute(query)
+                listaFestivos = comandos.fetchall()
 
-            contadorFestivos = 0
+                contadorFestivos = 0
 
-            for dia in listaDiasHorario:
-                for diaF in listaFestivos:
-                    if dia==diaF[0]:
-                        listaReporte.pop(contadorFestivos)
-                        listaReporte.insert(contadorFestivos,[dia.strftime("%Y-%m-%d"),"Festivo","",8])
-                contadorFestivos += 1
+                for dia in listaDiasHorario:
+                    for diaF in listaFestivos:
+                        if dia==diaF[0]:
+                            listaReporte.pop(contadorFestivos)
+                            listaReporte.insert(contadorFestivos,[dia.strftime("%Y-%m-%d"),"Festivo","",8])
+                    contadorFestivos += 1
 
-            return listaReporte
-
+                return listaReporte
+            except Exception as e:
+                messagebox.showerror("¡Error!","No se pudo generar la lista de asistencia." + str(e))
         else:
             messagebox.showerror("¡Error!","Selección de fechas inválida.")
 
